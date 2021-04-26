dc82518841cf4920681d7e21b80641105cc6033c
Yusuf ANI <yusufani8@gmail.com>
2021-04-26 22:42:20 +0300

    Added Code

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="yusufani",
+    name="TurkishPreprocessor",
     version="0.0.1",
     author="Yusuf ANI",
     author_email="yusufani8@gmail.com",
     description="A Turkish Preprocessor for NLP",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/yusufani/TurkishPreprocessor",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
